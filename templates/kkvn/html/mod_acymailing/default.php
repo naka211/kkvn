e0d468ffb92cb181c941c9de07d3b0e4efaaa1d7
Trung <nttrung211@yahoo.com>
2015-09-14 10:44:30 +0700

    delete acymailing

--- templates/kkvn/html/mod_acymailing/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('_JEXEC') or die('Restricted access');
-JHTML::_('behavior.formvalidator');
-?>
-<form id="<?php echo $formName; ?>" action="<?php echo JRoute::_('index.php'); ?>" method="post" name="<?php echo $formName ?>" class="form-validate" >
-	<input type="text" placeholder="Tên" class="input-cl form-control required">
-	<input type="text" placeholder="Email" class="input-cl form-control required" style="margin-top:10px;">
-	<button type="submit" class="go-btn btn submit-small-btn validate">Search</button>
-</form>
--- templates/kkvn/index.php
+++ templates/kkvn/index.php
@@ -123,6 +123,8 @@ $userProfile = JUserHelper::getProfile( $user->id );
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="<?php echo JURI::base();?>media/jui/js/jquery-migrate.min.js"></script>
+<script src="<?php echo JURI::base();?>media/system/js/validate.js"></script> 
 <script src="<?php echo $tmpl;?>js/bootstrap.min.js"></script> 
 <script type="text/javascript" src="<?php echo $tmpl;?>js/jquery.montage.min.js"></script>
 <script type="text/javascript" src="<?php echo $tmpl;?>js/moment.js"></script>
